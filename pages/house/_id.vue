1c43b52c36e16a9abc5adefc840e602fa812bf6c
bidavid <david.bilic9398@gmail.com>
2020-10-11 23:36:41 +0200

    bug ehheheehhehehe

--- pages/house/_id.vue
+++ pages/house/_id.vue
@@ -1,7 +1,7 @@
 <template>
-<div class="px-5 py-24 mx-auto">
+<div class="px-5 py-20 mx-auto">
   <div v-if="houseInfo && houseInfo.basicInfo && houseInfo.agencyInfo" class="lg:w-4/6 mx-auto">
-    <img class="h-64 w-full rounded-lg object-cover object-center overflow-hidden" :src="`${houseInfo.basicInfo.imageUrl}`" @error="loadPlaceholder">
+    <img class="h-64 w-full rounded-lg object-cover object-center overflow-hidden" :src="`${houseInfo.basicInfo.imageUrl}`" @error="loadHousePlaceholder">
     <div class="p-3">
 
       <h2 class="house-address-title">{{ houseInfo.basicInfo.address }}, {{ houseInfo.basicInfo.city.title}}</h2>
@@ -10,37 +10,36 @@
       <div class="flex flex-wrap mt-2">
         <h1 v-if="isBuyActive" class="price-title sm:mb-0">{{formattedPrice}}£</h1>
         <h1 v-else class="price-title sm:mb-0">{{formattedPrice}}£ <span class="text-sm mb-3 text-customRed">/ week</span></h1>
-        <span class="houseroom-info sm:mr-3 pr-3 sm:ml-auto sm:border-r border-glitter">
-          <img class="icon" src="/images/bedroom.png">
+        <span class="house-houseroom-info sm:mr-3 pr-3 sm:ml-auto sm:border-r border-glitter">
+          <img class="houseroom-icon" src="/images/bedroom.png">
           <span class="text-gray-500">{{houseInfo.basicInfo.bedrooms}}</span>
         </span>
-        <span class="houseroom-info sm:mx-0">
-          <img class="icon" src="/images/bathroom.png">
+        <span class="house-houseroom-info sm:mx-0">
+          <img class="houseroom-icon" src="/images/bathroom.png">
           <span class="text-gray-500">{{houseInfo.basicInfo.bathrooms}}</span>
         </span>
       </div>
     </div>
 
     <div class="flex flex-col sm:flex-row mt-4 p-6 border border-purple-100 shadow rounded-lg bg-purple-100">
-      <div class="sm:w-1/3 sm:pr-6 sm:py-8 overflow-hidden">
-        <img class="w-20 h-20 rounded-full mx-auto object-cover object-center overflow-hidden" :src="`${houseInfo.agencyInfo.imageUrl}`" @error="loadHousePlaceholder">
-        <div class="flex flex-col items-center text-center justify-center">
-          <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">{{houseInfo.agencyInfo.title}}</h2>
-          <div class="w-full h-1 bg-purple-500 rounded mt-2 mb-4"></div>
-          <p class="text-sm font-semibold text-gray-900">{{houseInfo.agencyInfo.specialities}}</p>
-          <p class="text-xs text-purple-400 mt-3 cursor-pointer italic">{{houseInfo.agencyInfo.website}}</p>
-        </div>
+      <div class="sm:w-1/5 sm:pr-6 overflow-hidden">
+        <p class="text-lg mb-2 text-left font-medium text-gray-900">Features:</p>
+        <ul v-if="houseInfo.basicInfo.features.length > 0" class="list-disc flex flex-col pl-6 justify-center flex-wrap">
+          <li v-for="(feature,index) in houseInfo.basicInfo.features" :key="index" class="mr-auto text-gray-600" >{{feature.title}}</li>
+        </ul>
+
+        <p v-else class="w-full rounded-full mt-6 text-center mx-auto bg-purple-600 text-white font-semibold uppercase tracking-widest text-xs"> Not available </p>
       </div>
 
-      <div class="sm:w-2/3 sm:pl-6 sm:border-l border-gray-300 sm:border-t-0 border-t mt-4 sm:mt-0 text-center sm:text-left">
-        <p class="text-lg mb-4 text-left font-medium text-gray-900">Description:</p>
-        <p class="text-lg mb-4 text-justify text-gray-600">{{houseInfo.basicInfo.description}}</p>
+      <div class="sm:w-4/5 sm:pl-6 sm:border-l border-gray-300 sm:border-t-0 border-t mt-4 sm:mt-0 text-center sm:text-left">
+        <p class="text-lg mb-2 text-left font-medium text-gray-900">Description:</p>
+        <p class="text-lg text-justify text-gray-600">{{houseInfo.basicInfo.description}}</p>
       </div>
     </div>
 
-    <div class="w-full">
-      <img class="w-20 h-20 rounded-full mx-auto object-cover object-center overflow-hidden" :src="`${houseInfo.agencyInfo.imageUrl}`" @error="loadHousePlaceholder">
-      <div class="flex flex-col items-center text-center justify-center">
+    <div class="w-full mt-10">
+      <img class="w-20 h-20 rounded-full mx-auto object-cover object-center overflow-hidden" :src="`${houseInfo.agencyInfo.imageUrl}`" @error="loadAgencyPlaceholder">
+      <div class="flex flex-col items-center text-center w-2/5 mx-auto">
         <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">{{houseInfo.agencyInfo.title}}</h2>
         <div class="w-full h-1 bg-purple-500 rounded mt-2 mb-4"></div>
         <p class="text-sm font-semibold text-gray-900">{{houseInfo.agencyInfo.specialities}}</p>
@@ -118,11 +117,11 @@ export default {
   @apply tracking-widest text-lg font-semibold text-purple-600 mt-2 truncate;
 }
 
-.houseroom-info{
+.house-houseroom-info{
   @apply text-glitter inline-flex items-center text-sm mx-auto;
 }
 
-.icon{
+.houseroom-icon{
   @apply mr-1;
 }
 
